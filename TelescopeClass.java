ab50c5102c2bcbdccb9fc933b5846a3c62c054b5
LukaszGrzymala <60157296+LukaszGrzymala@users.noreply.github.com>
2020-03-04 20:13:22 +0100

    Add files via upload

---
+++ TelescopeClass.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class TelescopeClass{
+
+
+    public void TelescopeClass{
+        System.out.println("Podaj nazwę teleskopu: ");
+        Scanner telescopeClassScanner = new Scanner(System.in);
+        String telescopeName = telescopeClassScanner.next();
+        System.out.println("Podaj standard rozmiaru wyciągu w calach");
+        String focuserSize = telescopeClassScanner.next();
+        System.out.println("Podaj aperturę teleskopu w milimetrach");
+        Double telescopeAperture = telescopeClassScanner.nextDouble();
+        System.out.println("Podaj ogniskową teleskopu w milimetrach");
+        Double telescopeFocalLength = telescopeClassScanner.nextDouble();
+        
+
+    }
+}
